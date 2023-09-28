b740ecbd22304bd68686fcb5c9aaf8ef506dbd29
Caio Augusto Exteckoetter <caioexteckoetter0@gmail.com>
2023-09-28 20:22:18 -0300

    teste

--- src/controllers/customersController.ts
+++ src/controllers/customersController.ts
@@ -1,12 +1,10 @@
 import * as customersModel from "../models/customersModel";
 
-const getCustomers = async (_req, res) => {
+const getCustomers = async (req, res) => {
     const customers = await customersModel.getCustomers();
 
     return res.status(200).json(customers.rows);
 };
-
-
 
 const createCustomer = async (req, res) => {
     const customer = await customersModel.createCustomer(req.body);
